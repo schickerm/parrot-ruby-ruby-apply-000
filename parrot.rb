9c9b1953d70a283e2e4d207bfe71a73c3cf32d2a
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-27 16:53:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(squawk)
-  if 
-  puts squawk 
-  return  
-end 
-
-parrot("relax")
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
+end
